7d3f82597380609ecfd0fde0f3d4bb82388df580
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-07-11 18:23:51 +0200

    [add] feat button.addEventListener

--- js/script.js
+++ js/script.js
@@ -25,13 +25,13 @@ numbersList.appendChild(listItem5);
 
 let seconds = 10;
 const countdown = document.getElementById('countdown');
-const answerForm = document.getElementById('answers-form');
+const answersForm = document.getElementById('answers-form');
 const instructions = document.getElementById('instructions');
 const idInterval = setInterval(() => {
   if (seconds === 0) {
     clearInterval(idInterval);
-    answerForm.classList.remove('d-none');
-    answerForm.classList.add('d-inline-block');
+    answersForm.classList.remove('d-none');
+    answersForm.classList.add('d-inline-block');
     countdown.classList.add('d-none');
     numbersList.classList.add('d-none');
     instructions.innerText = 'Inserisci i numeri che ti ricordi nell\'ordine che vuoi';
@@ -41,3 +41,32 @@ const idInterval = setInterval(() => {
   }
   seconds--;
 }, 1000);
+
+let arrayNumbers = [];
+
+arrayNumbers.push(listItem1.innerText);
+arrayNumbers.push(listItem2.innerText);
+arrayNumbers.push(listItem3.innerText);
+arrayNumbers.push(listItem4.innerText);
+arrayNumbers.push(listItem5.innerText);
+console.log(arrayNumbers)
+
+const button = document.querySelector('.btn');
+button.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const num1 = document.getElementById('num1').value;
+  const num2 = document.getElementById('num2').value;
+  const num3 = document.getElementById('num3').value;
+  const num4 = document.getElementById('num4').value;
+  const num5 = document.getElementById('num5').value;
+
+  let arrayAnswers = [];
+  arrayAnswers.push(num1);
+  arrayAnswers.push(num2);
+  arrayAnswers.push(num3);
+  arrayAnswers.push(num4);
+  arrayAnswers.push(num5);
+  console.log(arrayAnswers)
+})
+
