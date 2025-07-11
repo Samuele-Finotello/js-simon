42f406f1b06a85f1da20c2854a1b10dc328dcc2a
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-07-11 17:15:11 +0200

    [add] feat create tag li

--- js/script.js
+++ js/script.js
@@ -3,3 +3,11 @@ const numRandom = () => {
   return num;
 }
 
+const numbersList = document.getElementById('numbers-list');
+
+const listItem1 = document.createElement('li');
+const listItem2 = document.createElement('li');
+const listItem3 = document.createElement('li');
+const listItem4 = document.createElement('li');
+const listItem5 = document.createElement('li');
+
